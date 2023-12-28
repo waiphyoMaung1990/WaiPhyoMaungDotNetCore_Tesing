26cacae3318bd4ae4ebe4d0c909f947b39194a69
ko phyo <taryarphyo1990@gmail.com>
2023-12-28 08:24:19 +0630

    [Change] Name

--- WaiphyomaungDotNetCore.ConsoleApp/Models/BlogDataModel.cs
+++ WaiphyomaungDotNetCore.ConsoleApp/Models/BlogDataModel.cs
@@ -14,8 +14,8 @@ namespace WaiphyomaungDotNetCore.ConsoleApp.Models
     {
         [Key]
         public int Blog_Id { get; set; }
-        public string Blog_Title { get; set; }
-        public string Blog_Author { get; set; }
-        public string Blog_Content { get; set; }
+        public string? Blog_Title { get; set; }
+        public string? Blog_Author { get; set; }
+        public string? Blog_Content { get; set; }
     }
 }
